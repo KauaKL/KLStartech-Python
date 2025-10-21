249417efdd1c2041ee87c8e9b662c9008598fa3d
KauaKL <limakaua531@gmail.com>
2025-10-21 11:03:56 -0300

    atualizações no flet mobile

---
+++ env.py
@@ -0,0 +1,7 @@
+# Twilio (WhatsApp)
+"""
+TWILIO_ACCOUNT_SID=ACxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+TWILIO_AUTH_TOKEN=yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+TWILIO_WHATSAPP_FROM=whatsapp:+1415xxxxxxx
+ALERT_WHATSAPP_TO=whatsapp:+55xxxxxxxxxx  # opcional se inserir no app
+"""
